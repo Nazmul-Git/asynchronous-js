76efcf2417e8aa74f0f3f096c3187e9afd65d308
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2023-12-16 22:57:05 +0600

    Implement Asynchronous Javascript And XML (AJAX).

---
+++ script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+///////////////////////////////////////
+// XML http
+
+const request= new XMLHttpRequest();
+request.open('GET', 'https://restcountries.com/v3.1/name/bangladesh');
+const data= request.send();
+console.log(request.responseText)
+
+request.addEventListener('load', function(){
+    console.log(this.responseText);
+})
